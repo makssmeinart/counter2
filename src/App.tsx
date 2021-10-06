d1328fadb8c49a8bcf2c42b5e32d89d2c89ce526
makssmeinart <m.meinarts10@gmail.com>
2021-10-06 17:50:31 +0300

    Main Logic Added, No Errors & Disabling Buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {CounterSettings} from "./components/counterSettings/CounterSettings";
+import {Counter} from "./components/counter/Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export type minMaxType = {
+    min: number,
+    max: number
 }
 
-export default App;
+export const App = () => {
+    // UseState
+    const [minMax, setMinMax] = useState<minMaxType>({max: 10, min: 0})
+    const [counter, setCounter] = useState(minMax.min)
+
+
+    ///// Counter Settings Store //////
+    // Controlled MinMax inputs
+    const changeMinValue = (value: number) => {
+        setMinMax({...minMax, min: value})
+    }
+    const changeMaxValue = (value: number) => {
+        setMinMax({...minMax, max: value})
+    }
+    // on button set set minMax values to localStorage
+    const setMinMaxLocalStorage = () => {
+        localStorage.setItem("minMaxCounterValue", JSON.stringify(minMax))
+        setCounter(minMax.min)
+    }
+    // When load the page set minMax to localStorage if its not there
+    useEffect(() => {
+        if (localStorage.getItem("minMaxCounterValue") === null) {
+            localStorage.setItem("minMaxCounterValue", JSON.stringify(minMax))
+        }
+        else {
+            const data = localStorage.getItem("minMaxCounterValue")
+
+            if(data) {
+                setMinMax(JSON.parse(data))
+                setCounter(JSON.parse(data).min)
+            }
+
+        }
+    }, [])
+
+    ///// Counter State /////
+    const increaseCounterValue = () => {
+        // Getting minMax from localStorage
+        const minMax= localStorage.getItem("minMaxCounterValue")
+        // Checking that it exists there
+        if(minMax) {
+            const minMaxValue = JSON.parse(minMax)
+
+            if(counter < minMaxValue.max) setCounter(counter + 1)
+        }
+    }
+
+    const decreaseCounterValue = () => {
+        // Getting minMax from localStorage
+        const minMax = localStorage.getItem("minMaxCounterValue")
+        // Checking that it exists there
+        if(minMax) {
+            const minMaxValue = JSON.parse(minMax)
+
+            if(counter > minMaxValue.min) setCounter(counter - 1)
+        }
+    }
+
+
+    return (
+        <div>
+            <CounterSettings minMax={minMax} changeMinValue={changeMinValue} changeMaxValue={changeMaxValue}
+                             setMinMaxLocalStorage={setMinMaxLocalStorage}/>
+            <Counter counter={counter} increaseCounterValue={increaseCounterValue}
+                     decreaseCounterValue={decreaseCounterValue}/>
+        </div>
+    )
+}
